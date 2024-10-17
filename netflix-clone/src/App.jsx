428377d55fe29f4870ef7b4c3ac579f2a32a513d
Yakob Dereje <yakobdereje.yd@gmail.com>
2024-10-17 13:20:29 -0700

    first commit

---
+++ netflix-clone/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1> Hellow I Am Just Working Fine!! </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
